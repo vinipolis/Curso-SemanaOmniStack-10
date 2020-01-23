beb43e56fa8bc6f0d66abf373692e7ead8d37d54
vinipolis <viniciusnyw@hotmail.com>
2020-01-22 23:51:23 -0300

    Aula 2 - Criando a base da aplicação (Part2)

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,48 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(req, res) {
+        const devs = await Dev.find()
+
+        return res.json(devs)
+    },
+
+    async store(req, res) {
+        const { github_username, techs, latitude, longitude } = req.body
+
+        let dev = await Dev.findOne({ github_username })
+        if (!dev) {
+            const apiRes = await axios.get(`https://api.github.com/users/${github_username}`)
+
+            const { name = login, avatar_url, bio } = apiRes.data
+
+            const techsArray = parseStringAsArray(techs)
+
+            const location = {
+                type: "Point",
+                coordinates: [longitude, latitude]
+            }
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            })
+        }
+
+        return res.json(dev)
+    }
+}
+
+
+
+// index: mostrar um lista
+// show: mostra um unico
+// store: para criar
+// update: alterar
+// destroy: deletar
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,13 @@
 const { Router } = require('express')
-const axios = require('axios')
-const Dev = require('./models/Dev')
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router()
 
-routes.post('/devs', async (req, res) => {
-    const { github_username, techs } = req.body
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
 
-    const apiRes =  await axios.get(`https://api.github.com/users/${github_username}`)
-    
-    const { name = login, avatar_url, bio } = (apiRes.data)
-    
-    const techsArray = techs.split(',').map(tech => tech.trim())
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray
-    })
-
-    return res.json(dev)
-})
+routes.get('/search', SearchController.index)
 
 module.exports = routes
 
